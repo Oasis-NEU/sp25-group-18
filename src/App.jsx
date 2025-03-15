23ae734c00bd1b1984a4eab2500086a9a46daa19
officialtranmichael <officialtranmichael@gmail.com>
2025-03-15 17:30:13 -0400

    Made the home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "../src/components/Navbar/navbar";
-//import Home from "./pages/Home";
+import Home from "../src/pages/Home/Home";
 import Events from "../src/pages/Events/Events";
 import Create from "../src/pages/Create/Create";
 //import Signup from "./pages/Signup";
@@ -12,6 +12,7 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
+      <Route path="/home" element={<Home />} />
         <Route path="/events" element={<Events />} />
         <Route path="/create" element={<Create />} />
       </Routes>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,1 +1,47 @@
-"comment"
+import React from 'react';
+
+import './Home.css';
+
+function Home() {
+  return (
+    <div className="home-container">
+      <header className="header">
+        <h1>Study Buddy</h1>
+        <p>Connect, collaborate, and succeed together.</p>
+      </header>
+
+      <section className="who-we-are">
+        <h2>Who We Are</h2>
+        <p>
+          We are a community of students who organize study sessions, support
+          each other, and make learning more engaging.
+        </p>
+      </section>
+
+      <section className="get-started">
+        <h2>Get Started</h2>
+        <p>Join us today and enhance your learning experience.</p>
+        <button className="cta-button">Sign Up</button>
+      </section>
+
+      {/* Testimonials Section */}
+      <section className="testimonials">
+        <h2>What Students Say</h2>
+        <div className="testimonial">
+          <p>"Study Buddy helped me stay on track with my coursework!"</p>
+          <h4>- Alex Johnson</h4>
+        </div>
+        <div className="testimonial">
+          <p>"I met amazing study partners and improved my grades!"</p>
+          <h4>- Michael Tran</h4>
+        </div>
+        <div className="testimonial">
+          <p>"The study sessions are super organized and helpful."</p>
+          <h4>- Ryan Ma</h4>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default Home;
